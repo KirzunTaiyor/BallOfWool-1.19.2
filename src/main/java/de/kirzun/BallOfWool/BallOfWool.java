42017d96a9af8b1878bc085a62176c02c22260b4
KirzunTaiyor <andrehoelbe@hotmail.de>
2022-12-13 23:05:38 +0100

    New

---
+++ src/main/java/de/kirzun/BallOfWool/BallOfWool.java
@@ -0,0 +1,40 @@
+package de.kirzun.BallOfWool;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(BallOfWool.MOD_ID)
+public class BallOfWool {
+// Change
+    //new line
+
+    public static final String MOD_ID = "ballofwool";
+ final Logger LOGGER = LogUtils.getLogger();
+
+    public static void main(String[] args) {
+        System.out.println("Hallo");
+    }
+
+    public BallOfWool() {
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
