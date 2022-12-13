782724d4e4f972f61bf0d7fd274dd4c0a903a433
KirzunTaiyor <andrehoelbe@hotmail.de>
2022-12-13 23:12:51 +0100

    Start Coding...

--- src/main/java/de/kirzun/BallOfWool/BallOfWool.java
+++ src/main/java/de/kirzun/BallOfWool/BallOfWool.java
@@ -11,9 +11,7 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(BallOfWool.MOD_ID)
 public class BallOfWool {
-// Change
-    //new line
-    //new
+
 
     public static final String MOD_ID = "ballofwool";
  final Logger LOGGER = LogUtils.getLogger();
